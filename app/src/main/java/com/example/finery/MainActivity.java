01fe5d9d26a5417afb303053e4f852f59c04090f
perusha <perushawanniarachchi22@gmail.com>
2020-09-22 11:35:39 +0530

    added new activity

--- app/src/main/java/com/example/finery/MainActivity.java
+++ app/src/main/java/com/example/finery/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.finery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCart = (Button)findViewById(R.id.btnCart);
+    }
+
+
+    public void onClick(View view) {
+        Intent i;
+
+        switch(view.getId())
+        {
+            case R.id.btnCart : i=new Intent(this,CartActivity.class);startActivity(i);break;
+
+
+
+            default:break;
+        }
+
     }
 }
