bbbd7c597d474a03cdc9d99e8254b0cfc946e102
perusha <perushawanniarachchi22@gmail.com>
2020-09-25 20:58:55 +0530

    next2 commit

--- app/src/main/java/com/example/finery/CartActivity.java
+++ app/src/main/java/com/example/finery/CartActivity.java
@@ -1,13 +1,20 @@
 package com.example.finery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CartActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button NextProcessBtn;
+    private TextView txtTotalAmount;
 
     private Button btnDel;
     @Override
@@ -15,6 +22,8 @@ public class CartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
         btnDel = (Button)findViewById(R.id.button2);
+
+
     }
     public void onClick(View view) {
         Intent i;
